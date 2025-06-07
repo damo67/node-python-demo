7b989faa5b4b16915d05808df8443fea2749551f
Damo67 <damian.guiney@gmail.com>
2025-06-07 12:21:54 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// app.js
+const { spawn } = require('child_process');
+
+const py = spawn('python', ['hello.py']);
+
+py.stdout.on('data', (data) => {
+  console.log(`Python says: ${data}`);
+});
+
+py.stderr.on('data', (data) => {
+  console.error(`Error: ${data}`);
+});
+
+py.on('close', (code) => {
+  console.log(`Python process exited with code ${code}`);
+});
